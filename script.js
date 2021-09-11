7bed36b9c5e23ea3c916782b47506b27735f26cc
theo gandara <theogandara@gmail.com>
2021-09-11 17:45:14 -0300

    realizado as funções

--- script.js
+++ script.js
@@ -1,1 +1,53 @@
+function getRandom() {
+    return Math.floor(Math.random() * 6 + 1)
+}
 
+
+function somando(){
+    
+    let somaDosDados = 0 
+    
+    for(let i = 0; i < 2 ;i++){
+        
+        somaDosDados += (getRandom())
+    }
+    return somaDosDados
+}
+
+
+let count = [0,0,0,0,0,0,0,0,0,0,0,0,0]
+
+for(let j = 0; j < 1000 ; j++){
+    
+    doisDados = somando()
+    
+    count[doisDados] = count[doisDados] + 1;
+
+}
+
+console.log(count)
+
+
+for ( let c = 2; c < count.length; c++ ){
+
+    
+    const divSelecionada = document.getElementById('barras')
+
+    const barra = document.createElement('div')
+    
+    barra.innerText =  c;
+
+    divSelecionada.appendChild(barra)
+
+    barra.style.width = `${count[c]}px`;
+
+    barra.style.backgroundColor = `#ee7441`;
+
+    barra.style.color = `#00008b`;
+
+    barra.style.height = `40px`;
+
+    barra.style.margin = `10px auto`;
+
+    barra.style.textAlign = `center`;
+}
